374210d8a0bf68e8f70cc0183535dc994fabccf7
Aaron Ge <aaronge13@icloud.com>
2023-09-18 14:03:55 -0400

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,4 +1,4 @@
-import { TransformerModel } from "./transformer.js";
+import { TransformerModel } from "./transformerv2.js";
 import { processJson, detokenizeSentence } from "./tokenizer.js";
 
 export {
